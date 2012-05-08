8d107292cae51770d2b0b26482fbcc83d3a1b2ab
Keith Bloom <keith.bloom@gmail.com>
2012-05-08 22:21:43 +0100

    Finished deploy script

--- AdventureWeb/Src/AdventureWeb/AdventureWorks.Database/deployment/LOCAL.DBDeployment.bat
+++ AdventureWeb/Src/AdventureWeb/AdventureWorks.Database/deployment/LOCAL.DBDeployment.bat
@@ -1,21 +1,15 @@
 :: AdventureWorks
-@echo off
+::@echo off
 
 SET DIR=%~d0%~p0%
 
 SET database.name="AdventureWorks"
 SET sql.files.directory="..\db\"
-SET server.database=".\Sql2008"
-::SET version.file="_BuildInfo.xml"
-::SET version.xpath="//buildInfo/version"
-SET environment="LOCAL"
-SET output=".\"
-SET rh.path= "%DIR%..\..\..\Tools\RoundhousE\console\"
+SET server.database=".\Sql2008R2"
+SET rh.path= "%DIR%..\..\..\..\Tools\RoundhousE\console\"
 SET restore.path=%DIR%..\..\..\DatabaseBackups\
 SET restore.name=AdventureWorks.bak
 
 :: Run RH in RestoreRun mode
 %rh.path%\rh.exe /s=%server.database% /d=%database.name% -f ..\db --debug /restore /rfp=%restore.path%\%restore.name% /simple
 
-:: Run RH in normal mode
-::%rh.path%\rh.exe /s=%server.database% /d=%database.name% -f ..\db --debug
